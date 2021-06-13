f5bc6adbfaa907e9cb03892dc0cf1b24f549099f
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-06-13 17:28:04 +0700

    No.6

---
+++ No.6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace No._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            while (check == false)
+                {
+                Console.WriteLine("Input : ");
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please input a number from 0 - 3 to choose the following menu:");
+                if (input == 0)
+                {
+                    Console.WriteLine("Show all recent files.");
+                }
+                else if (input == 1)
+                {
+                    Console.WriteLine("Open new file.");
+
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("Open recent file");
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine("End");
+                    check = true;
+                }
+            }
+        }
+    }
+}
