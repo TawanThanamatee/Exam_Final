deffc1fdeaf943529b95640079ef59f5b41818dc
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-06-13 14:50:43 +0700

    No.1

---
+++ No.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace No._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Number = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Number[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(Number[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
