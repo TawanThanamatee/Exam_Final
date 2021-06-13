30e58cd96594f497c57570654dd4fe96c504eb49
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-06-13 16:44:23 +0700

    No.2

---
+++ No.2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace No._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Many Number : ");
+            int Many_Number = int.Parse(Console.ReadLine());
+
+            int[] Many_Number_Array = new int[Many_Number];
+
+            for (int i = 0; i < Many_Number; i++)
+            {
+                Console.Write("Number : ");
+                Many_Number_Array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < Many_Number; i++)
+            {
+                for (int j = i + 1; j < Many_Number; j++)
+                {
+                    if (Many_Number_Array[j] < Many_Number_Array[i])
+                    {
+                        int deposit = Many_Number_Array[i];
+                        Many_Number_Array[i] = Many_Number_Array[j];
+                        Many_Number_Array[j] = deposit;
+                    }
+                }
+            }
+            for (int i = 0; i < Many_Number; i++)
+            {
+                Console.Write("Answer : ");
+                Console.Write(Many_Number_Array[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
